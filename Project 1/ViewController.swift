29a059ed141bb2d3a5a88e099bf07f7be6327b07
julienlegault <julien.p.legault@gmail.com>
2024-01-11 17:38:55 -0500

    Completed project 1 with errors

---
+++ Project 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 1
+//
+//  Created by user250832 on 1/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
